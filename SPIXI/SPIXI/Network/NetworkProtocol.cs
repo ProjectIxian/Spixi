4b64fd0dceed2660185b58ed7bb65e09f81aabb9
Cristian Ciorba <cristian@firestorm.ro>
2018-11-02 00:29:19 +0200

    - partial update of SPIXI code

--- SPIXI/SPIXI/Network/NetworkProtocol.cs
+++ SPIXI/SPIXI/Network/NetworkProtocol.cs
@@ -5,6 +5,7 @@ using SPIXI.Storage;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -13,91 +14,54 @@ namespace DLT.Network
 {
     public class ProtocolMessage
     {
-
-        // Prepare a network protocol message. Works for both client-side and server-side
-        public static byte[] prepareProtocolMessage(ProtocolMessageCode code, byte[] data)
-        {
-            byte[] result = null;
-
-            // Prepare the protocol sections
-            int data_length = data.Length;
-            byte[] data_checksum = Crypto.sha256(data);
-
-            using (MemoryStream m = new MemoryStream())
-            {
-                using (BinaryWriter writer = new BinaryWriter(m))
-                {
-                    // Protocol sections are code, length, checksum, data
-                    // Write each section in binary, in that specific order
-                    writer.Write((int)code);
-                    writer.Write(data_length);
-                    writer.Write(data_checksum);
-                    writer.Write(data);
-                }
-                result = m.ToArray();
-            }
-
-            return result;
-        }
-
         // Broadcast a protocol message across clients and nodes
-        public static void broadcastProtocolMessage(ProtocolMessageCode code, byte[] data, Socket skipSocket = null)
+        // Returns true if it sent the message at least one endpoint. Returns false if the message couldn't be sent to any endpoints
+        public static bool broadcastProtocolMessage(ProtocolMessageCode code, byte[] data, RemoteEndpoint skipEndpoint = null, bool sendToSingleRandomNode = false)
         {
             if (data == null)
             {
                 Logging.warn(string.Format("Invalid protocol message data for {0}", code));
-                return;
+                return false;
             }
 
-            // Skip presence updates
-            if(code == ProtocolMessageCode.updatePresence || code == ProtocolMessageCode.removePresence || code == ProtocolMessageCode.syncPresenceList)
+            if (sendToSingleRandomNode)
             {
-                return;
+                int serverCount = NetworkClientManager.getConnectedClients().Count();
+
+                Random r = new Random();
+                int rIdx = r.Next(serverCount);
+
+                RemoteEndpoint re = null;
+
+                re = NetworkClientManager.getClient(rIdx);
+                
+                if (re != null && re.isConnected())
+                {
+                    re.sendData(code, data);
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                bool c_result = NetworkClientManager.broadcastData(code, data, skipEndpoint);
+
+                if (!c_result)
+                    return false;
             }
 
-            NetworkClientManager.broadcastData(code, data);
-            //NetworkServer.broadcastData(code, data);
+            return true;
         }
 
-        // Server-side protocol reading
-        public static void readProtocolMessage(Socket socket)
-        {
-            // Check for socket availability
-            if (socket.Connected == false)
-            {
-                throw new Exception("Socket already disconnected at other end");
-            }
 
-            if (socket.Available < 1)
+        // Read a protocol message from a byte array
+        public static void readProtocolMessage(byte[] recv_buffer, RemoteEndpoint endpoint)
+        {
+            if (endpoint == null)
             {
-                // Sleep a while to prevent cpu cycle waste
-                Thread.Sleep(100);
+                Logging.error("Endpoint was null. readProtocolMessage");
                 return;
             }
-            // Read multi-packet messages
-            // TODO: optimize this as it's not very efficient
-            var big_buffer = new List<byte>();
-
-            try
-            {
-                while (socket.Available > 0)
-                {
-                    var current_byte = new Byte[1];
-                    var byteCounter = socket.Receive(current_byte, current_byte.Length, SocketFlags.None);
-
-                    if (byteCounter.Equals(1))
-                    {
-                        big_buffer.Add(current_byte[0]);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("NET: endpoint disconnected " + e);
-                throw e;
-            }
-
-            byte[] recv_buffer = big_buffer.ToArray();
 
             ProtocolMessageCode code = ProtocolMessageCode.hello;
             byte[] data = null;
@@ -109,52 +73,72 @@ namespace DLT.Network
                     // Check for multi-message packets. One packet can contain multiple network messages.
                     while (reader.BaseStream.Position < reader.BaseStream.Length)
                     {
-                        int message_code = reader.ReadInt32();
-                        code = (ProtocolMessageCode)message_code;
-
-                        int data_length = reader.ReadInt32();
-                        if (data_length < 0)
-                            return;
-
                         byte[] data_checksum;
-
                         try
                         {
+                            byte startByte = reader.ReadByte();
+
+                            int message_code = reader.ReadInt32();
+                            code = (ProtocolMessageCode)message_code;
+
+                            int data_length = reader.ReadInt32();
+
+                            // If this is a connected client, filter messages
+                            if (endpoint.GetType() == typeof(RemoteEndpoint))
+                            {
+                                if (endpoint.presence == null)
+                                {
+                                    // Check for presence and only accept hello and syncPL messages if there is no presence.
+                                    if (code == ProtocolMessageCode.hello || code == ProtocolMessageCode.syncPresenceList || code == ProtocolMessageCode.getBalance || code == ProtocolMessageCode.newTransaction)
+                                    {
+
+                                    }
+                                    else
+                                    {
+                                        // Ignore anything else
+                                        return;
+                                    }
+                                }
+                            }
+
+
+
+
                             data_checksum = reader.ReadBytes(32); // sha256, 8 bits per byte
+                            byte header_checksum = reader.ReadByte();
+                            byte endByte = reader.ReadByte();
                             data = reader.ReadBytes(data_length);
                         }
                         catch (Exception e)
                         {
-                            Console.WriteLine("NET: dropped packet. " + e);
+                            Logging.error(String.Format("NET: dropped packet. {0}", e));
                             return;
                         }
                         // Compute checksum of received data
-                        byte[] local_checksum = Crypto.sha256(data);
+                        byte[] local_checksum = Crypto.sha512sqTrunc(data);
 
                         // Verify the checksum before proceeding
-                        if (Crypto.byteArrayCompare(local_checksum, data_checksum) == false)
+                        if (local_checksum.SequenceEqual(data_checksum) == false)
                         {
-                            Logging.warn("Dropped message (invalid checksum)");
+                            Logging.error("Dropped message (invalid checksum)");
                             continue;
                         }
 
                         // For development purposes, output the proper protocol message
-                        Console.WriteLine(string.Format("NET: {0} | {1} | {2}", code, data_length, Crypto.hashToString(data_checksum)));
+                        //Console.WriteLine(string.Format("NET: {0} | {1} | {2}", code, data_length, Crypto.hashToString(data_checksum)));
 
                         // Can proceed to parse the data parameter based on the protocol message code.
                         // Data can contain multiple elements.
-                        parseProtocolMessage(code, data, socket);
+                        //parseProtocolMessage(code, data, socket, endpoint);
+                        NetworkQueue.receiveProtocolMessage(code, data, data_checksum, endpoint);
                     }
                 }
             }
-
-
-
-
         }
 
+
         // Unified protocol message parsing
-        public static void parseProtocolMessage(ProtocolMessageCode code, byte[] data, Socket socket)
+        public static void parseProtocolMessage(ProtocolMessageCode code, byte[] data, RemoteEndpoint endpoint)
         {
             try
             {
---
+++ SPIXI/SPIXI/Network/NetworkServer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+// Dummy Network server for IXICore
+namespace DLT.Network
+{
+    public class NetworkServer
+    {
+        // Returns all the connected clients
+        public static string[] getConnectedClients(bool useIncomingPort = false)
+        {
+            List<String> result = new List<String>();
+            return result.ToArray();
+        }
+    }
+}
