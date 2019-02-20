5e1f02930baf38342b2c71196ffd2f28340f926d
Cristian Ciorba <cristian@firestorm.ro>
2019-02-20 18:18:02 +0200

    - added Spixi intro retry screen for when the cached wallet password is not found - updated Spixi UI assets

---
+++ SPIXI/SPIXI/Pages/Launch/LaunchRetryPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using SPIXI.Interfaces;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using DLT.Meta;
+
+namespace SPIXI
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LaunchRetryPage : SpixiContentPage
+    {
+		public LaunchRetryPage ()
+		{
+			InitializeComponent ();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            // Load the platform specific home page url
+            var source = new UrlWebViewSource();
+            source.Url = string.Format("{0}html/intro_retry.html", DependencyService.Get<IBaseUrl>().Get());
+            webView.Source = source;
+        }
+
+        private void onNavigated(object sender, WebNavigatedEventArgs e)
+        {
+
+        }
+
+        private void onNavigating(object sender, WebNavigatingEventArgs e)
+        {
+            string current_url = e.Url;
+
+            if (current_url.Equals("ixian:back", StringComparison.Ordinal))
+            {
+                Navigation.PopAsync(Config.defaultXamarinAnimations);
+            }
+            else if (current_url.Equals("ixian:proceed", StringComparison.Ordinal))
+            {
+                //Navigation.PushAsync(new LaunchRestorePage(), Config.defaultXamarinAnimations);
+            }
+            else
+            {
+                // Otherwise it's just normal navigation
+                e.Cancel = false;
+                return;
+            }
+            e.Cancel = true;
+
+        }
+    }
+}
