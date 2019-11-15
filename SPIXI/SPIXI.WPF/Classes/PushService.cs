0dc96e42599d4c4f4e6c965a575e9aa13372fde2
firestorm40 <cristian@firestorm.ro>
2019-11-15 20:48:29 +0200

    - fixed WPF push service dependency

---
+++ SPIXI/SPIXI.WPF/Classes/PushService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPIXI.Interfaces;
+using Xamarin.Forms;
+
+
+[assembly: Dependency(typeof(PushService_WPF))]
+
+public class PushService_WPF : IPushService
+{
+    public void initialize()
+    {
+
+    }
+
+    public void setTag(string tag)
+    {
+
+    }
+
+}
