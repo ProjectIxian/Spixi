5f5a05bf988de1a6dc9e9daf23341c147e3f50ff
MarkoAT <no-reply@ixian.io>
2020-03-12 02:14:01 +0100

    - added clear notifications when viewing contacts screen

--- SPIXI/SPIXI/Pages/Home/HomePage.xaml.cs
+++ SPIXI/SPIXI/Pages/Home/HomePage.xaml.cs
@@ -229,7 +229,7 @@ namespace SPIXI
             else if (current_url.Contains("ixian:tab:"))
             {
                 currentTab = current_url.Split(new string[] { "ixian:tab:" }, StringSplitOptions.None)[1];
-                if(currentTab == "tab2")
+                if(currentTab == "tab2" || currentTab == "tab4")
                 {
                     // chat tab
                     DependencyService.Get<IPushService>().clearNotifications();
