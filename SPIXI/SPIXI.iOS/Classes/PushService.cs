4982236f5ed51b439bd5f25dc86e55af79bc1776
firestorm40 <cristian@firestorm.ro>
2020-03-31 22:42:41 +0300

    - added iOS push notification entitlements and project settings - changed iOS bundle display name to Spixi - added static registrar mtouch argument to prevent push notification crashes in the iOS simulator - added initial OneSignal support for iOS - added local push notifications support for iOS

--- SPIXI/SPIXI.iOS/Classes/PushService.cs
+++ SPIXI/SPIXI.iOS/Classes/PushService.cs
@@ -7,6 +7,10 @@ using System.Text;
 
 using Foundation;
 using UIKit;
+using Com.OneSignal;
+using Com.OneSignal.Abstractions;
+using SPIXI;
+using IXICore.Meta;
 
 [assembly: Dependency(typeof(PushService_iOS))]
 
@@ -14,21 +18,57 @@ public class PushService_iOS : IPushService
 {
     public void initialize()
     {
-
+        OneSignal.Current.StartInit(SPIXI.Meta.Config.oneSignalAppId)
+            .InFocusDisplaying(Com.OneSignal.Abstractions.OSInFocusDisplayOption.None)
+            .HandleNotificationReceived(handleNotificationReceived)
+            .HandleNotificationOpened(handleNotificationOpened)
+            .EndInit();
     }
 
     public void setTag(string tag)
     {
-
+        OneSignal.Current.SendTag("ixi", tag);
     }
 
     public void clearNotifications()
     {
-
+        UIApplication.SharedApplication.ApplicationIconBadgeNumber = 0;
     }
 
     public void showLocalNotification(string title, string message, string data)
     {
+        var notification = new UILocalNotification();
+        notification.FireDate = NSDate.FromTimeIntervalSinceNow(1);
+        notification.AlertAction = title;
+        notification.AlertBody = message;
+        notification.ApplicationIconBadgeNumber = 1;
+        notification.SoundName = UILocalNotification.DefaultSoundName;
+        UIApplication.SharedApplication.ScheduleLocalNotification(notification);
+    }
 
+
+    static void handleNotificationReceived(OSNotification notification)
+    {
+        OneSignal.Current.ClearAndroidOneSignalNotifications();
+        OfflinePushMessages.fetchPushMessages(true);
+    }
+
+    static void handleNotificationOpened(OSNotificationOpenedResult inNotificationOpenedDelegate)
+    {
+        if (inNotificationOpenedDelegate.notification.payload.additionalData.ContainsKey("fa"))
+        {
+            var fa = inNotificationOpenedDelegate.notification.payload.additionalData["fa"];
+            if (fa != null)
+            {
+                try
+                {
+                    App.startingScreen = Convert.ToString(fa);
+                }
+                catch (Exception e)
+                {
+                    Logging.error("Exception occured in handleNotificationOpened: {0}", e);
+                }
+            }
+        }
     }
 }
