ec509d5840cd117a8ce0a8fffcb5027efb9aad2f
MarkoAT <no-reply@ixian.io>
2020-04-08 23:41:51 +0200

    - added dummy classes for VoIP support

---
+++ SPIXI/SPIXI.iOS/Classes/AudioPlayeriOS.cs
@@ -0,0 +1,82 @@
+﻿using IXICore.Meta;
+using SPIXI.VoIP;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AudioPlayeriOS))]
+
+public class AudioPlayeriOS : IAudioPlayer
+{
+    private bool running = false;
+
+    public AudioPlayeriOS()
+    {
+        /*audioPlayer = new AudioTrack(
+            // Stream type
+            Android.Media.Stream.VoiceCall,
+            // Frequency
+            11025,
+            // Mono or stereo
+            ChannelOut.Mono,
+            // Audio encoding
+            Android.Media.Encoding.Pcm16bit,
+            // Length of the audio clip.
+            -1,
+            // Mode. Stream or static.
+            AudioTrackMode.Stream);*/
+
+
+    }
+
+    public void start()
+    {
+        if (running)
+        {
+            Logging.warn("Audio player is already running.");
+            return;
+        }
+
+        running = true;
+
+        // TODO Init player
+    }
+
+    public int write(byte[] audio_data, int offset_in_bytes, int size_in_bytes)
+    {
+        //return await audioPlayer.WriteAsync(audio_data, offset_in_bytes, size_in_bytes);
+        return 0;
+    }
+
+    public void pause()
+    {
+        /*if (audioPlayer == null)
+        {
+            return;
+        }
+
+        audioPlayer.Pause();*/
+    }
+
+    public void stop()
+    {
+        /*if (audioPlayer == null)
+        {
+            return;
+        }
+        audioPlayer.Stop();
+        audioPlayer.Release();
+        audioPlayer.Dispose();
+        audioPlayer = null;
+        running = false;*/
+    }
+
+    public void Dispose()
+    {
+        stop();
+    }
+
+    public bool isRunning()
+    {
+        return running;
+    }
+}
---
+++ SPIXI/SPIXI.iOS/Classes/AudioRecorderiOS.cs
@@ -0,0 +1,83 @@
+﻿using IXICore.Meta;
+using SPIXI.VoIP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AudioRecorderiOS))]
+
+public class AudioRecorderiOS : IAudioRecorder
+{
+    private Action<byte[]> OnSoundDataReceived;
+
+    bool stopRecording = false;
+    bool running = false;
+
+    public AudioRecorderiOS()
+    {
+
+    }
+
+    public void start()
+    {
+        if (running)
+        {
+            Logging.warn("Audio recorder is already running.");
+            return;
+        }
+        stopRecording = false;
+        running = true;
+
+        // TODO Init recorder
+
+        Thread recordingThread = new Thread(readLoop);
+        recordingThread.Start();
+    }
+
+    void readLoop()
+    {
+        byte[] buffer = new byte[8192];
+        while (!stopRecording)
+        {
+            try
+            {
+                //int num_bytes = await audioRecorder.ReadAsync(buffer, 0, buffer.Length);
+                //OnSoundDataReceived(buffer, 0, num_bytes);
+            }
+            catch (Exception e)
+            {
+                Logging.error("Exception occured while recording audio stream: " + e);
+                break;
+            }
+            Thread.Sleep(10);
+        }
+
+        // TODO Stop recorder and cleanup
+
+
+        running = false;
+    }
+    public void stop()
+    {
+        stopRecording = true;
+    }
+
+    public void Dispose()
+    {
+        stop();
+    }
+
+    public bool isRunning()
+    {
+        return running;
+    }
+
+    public void setOnSoundDataReceived(Action<byte[]> on_sound_data_received)
+    {
+        OnSoundDataReceived = on_sound_data_received;
+    }
+}
