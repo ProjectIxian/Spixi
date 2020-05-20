2eb8f4c5b16bfaa2d873ca1ec1c8055b70ead17e
MarkoAT <no-reply@ixian.io>
2020-05-20 16:41:58 +0200

    spixi-0.4.5

--- SPIXI/SPIXI/Meta/Config.cs
+++ SPIXI/SPIXI/Meta/Config.cs
@@ -28,7 +28,7 @@ namespace SPIXI.Meta
         public static readonly int packetDataSize = 102400; // 100 Kb per packet for file transfers
         public static readonly long packetRequestTimeout = 60; // Time in seconds to re-request packets
 
-        public static readonly string version = "spixi-0.4.5-dev"; // Spixi version
+        public static readonly string version = "spixi-0.4.5"; // Spixi version
 
         // Default SPIXI settings
         public static bool defaultXamarinAnimations = false;
