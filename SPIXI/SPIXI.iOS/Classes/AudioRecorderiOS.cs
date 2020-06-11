0f6f8aa3ca341b12e1cdfb6c49d2f4d01b61136a
MarkoAT <no-reply@ixian.io>
2020-06-11 02:30:34 +0200

    - implemented audio recorder for iOS

--- SPIXI/SPIXI.iOS/Classes/AudioRecorderiOS.cs
+++ SPIXI/SPIXI.iOS/Classes/AudioRecorderiOS.cs
@@ -1,21 +1,32 @@
-﻿using IXICore.Meta;
+﻿using AudioToolbox;
+using AVFoundation;
+using Foundation;
+using IXICore.Meta;
 using SPIXI.VoIP;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Runtime.InteropServices;
 using System.Threading;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(AudioRecorderiOS))]
 
-public class AudioRecorderiOS : IAudioRecorder
+public class AudioRecorderiOS : IAudioRecorder, IAudioEncoderCallback
 {
     private Action<byte[]> OnSoundDataReceived;
 
-    bool stopRecording = false;
+    private AVAudioEngine audioRecorder = null;
+    private IAudioEncoder audioEncoder = null;
+
     bool running = false;
+
+    List<byte[]> outputBuffers = new List<byte[]>();
+
+    Thread recordThread = null;
+
+    int sampleRate = SPIXI.Meta.Config.VoIP_sampleRate;
+    int bitRate = SPIXI.Meta.Config.VoIP_bitRate;
+    int channels = SPIXI.Meta.Config.VoIP_channels;
 
     public AudioRecorderiOS()
     {
@@ -29,41 +40,93 @@ public class AudioRecorderiOS : IAudioRecorder
             Logging.warn("Audio recorder is already running.");
             return;
         }
-        stopRecording = false;
         running = true;
 
-        // TODO Init recorder
+        lock (outputBuffers)
+        {
+            outputBuffers.Clear();
+        }
 
-        Thread recordingThread = new Thread(readLoop);
-        recordingThread.Start();
+        initEncoder(codec);
+        initRecorder();
+
+        recordThread = new Thread(recordLoop);
+        recordThread.Start();
     }
 
-    void readLoop()
+    private void initRecorder()
     {
-        byte[] buffer = new byte[8192];
-        while (!stopRecording)
+        audioRecorder = new AVAudioEngine();
+        NSError error = null;
+        AVAudioSession.SharedInstance().SetPreferredSampleRate(sampleRate, out error);
+        AVAudioFormat recording_format = new AVAudioFormat(AVAudioCommonFormat.PCMInt16, sampleRate, (uint)channels, true);
+        uint buffer_size = (uint)(40 * 4 * channels * sampleRate / 1000);
+        audioRecorder.InputNode.InstallTapOnBus(0, buffer_size, recording_format, onDataAvailable);
+        audioRecorder.Prepare();
+        audioRecorder.StartAndReturnError(out error);
+    }
+
+    private void onDataAvailable(AVAudioPcmBuffer buffer, AVAudioTime when)
+    {
+        AudioBuffer audioBuffer = buffer.AudioBufferList[0];
+        byte[] data = new byte[audioBuffer.DataByteSize];
+        Marshal.Copy(audioBuffer.Data, data, 0, audioBuffer.DataByteSize);
+        
+        encode(data, 0, data.Length);
+    }
+
+    private void initEncoder(string codec)
+    {
+        switch (codec)
+        {
+            case "opus":
+                initOpusEncoder();
+                break;
+
+            default:
+                throw new Exception("Unknown recorder codec selected " + codec);
+        }
+    }
+
+    private void initOpusEncoder()
+    {
+        audioEncoder = new OpusEncoder(sampleRate, 24000, 1, Concentus.Enums.OpusApplication.OPUS_APPLICATION_VOIP, this);
+        audioEncoder.start();
+    }
+
+    public void stop()
+    {
+        if (!running)
+        {
+            return;
+        }
+        running = false;
+
+        if (audioRecorder != null)
         {
             try
             {
-                //int num_bytes = await audioRecorder.ReadAsync(buffer, 0, buffer.Length);
-                //OnSoundDataReceived(buffer, 0, num_bytes);
+                audioRecorder.Stop();
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                Logging.error("Exception occured while recording audio stream: " + e);
-                break;
+
             }
-            Thread.Sleep(10);
+            audioRecorder.Dispose();
+            audioRecorder = null;
         }
 
-        // TODO Stop recorder and cleanup
+        if (audioEncoder != null)
+        {
+            audioEncoder.stop();
+            audioEncoder.Dispose();
+            audioEncoder = null;
+        }
 
-
-        running = false;
-    }
-    public void stop()
-    {
-        stopRecording = true;
+        lock (outputBuffers)
+        {
+            outputBuffers.Clear();
+        }
     }
 
     public void Dispose()
@@ -79,5 +142,91 @@ public class AudioRecorderiOS : IAudioRecorder
     public void setOnSoundDataReceived(Action<byte[]> on_sound_data_received)
     {
         OnSoundDataReceived = on_sound_data_received;
+    }
+
+    private void recordLoop()
+    {
+        while (running)
+        {
+            Thread.Sleep(10);
+
+            try
+            {
+                sendAvailableData();
+            }
+            catch (Exception e)
+            {
+                Logging.error("Exception occured while recording audio stream: " + e);
+            }
+        }
+        recordThread = null;
+    }
+
+    private void encode(byte[] buffer, int offset, int size)
+    {
+        if (!running)
+        {
+            return;
+        }
+        if (size > 0)
+        {
+            try
+            {
+                byte[] encoded_bytes = audioEncoder.encode(buffer, offset, size);
+                if (encoded_bytes != null)
+                {
+                    onEncodedData(encoded_bytes);
+                }
+            }
+            catch (Exception e)
+            {
+                Logging.error("Exception occured in encode loop: " + e);
+            }
+        }
+    }
+
+    private void sendAvailableData()
+    {
+        if (!running)
+        {
+            return;
+        }
+        byte[] data_to_send = null;
+        lock (outputBuffers)
+        {
+            int total_size = 0;
+            foreach (var buf in outputBuffers)
+            {
+                total_size += buf.Length;
+            }
+
+            if (total_size >= 400)
+            {
+                data_to_send = new byte[total_size];
+                int data_written = 0;
+                foreach (var buf in outputBuffers)
+                {
+                    Array.Copy(buf, 0, data_to_send, data_written, buf.Length);
+                    data_written += buf.Length;
+                }
+                outputBuffers.Clear();
+            }
+        }
+        if (data_to_send != null)
+        {
+            OnSoundDataReceived(data_to_send);
+        }
+    }
+
+    public void onEncodedData(byte[] data)
+    {
+        if (!running)
+        {
+            return;
+        }
+        lock (outputBuffers)
+        {
+            outputBuffers.Add(data);
+        }
     }
 }
