cb931283e075b9f3a087f02ad90d1feeb926918e
MarkoAT <no-reply@ixian.io>
2020-06-11 02:28:33 +0200

    - added iOS microphone permissions

--- SPIXI/SPIXI.iOS/Classes/SpixiPermissions.cs
+++ SPIXI/SPIXI.iOS/Classes/SpixiPermissions.cs
@@ -1,6 +1,7 @@
 ﻿using SPIXI.iOS.Classes;
 using SPIXI.Interfaces;
 using Xamarin.Forms;
+using AVFoundation;
 
 [assembly: Dependency(typeof(SpixiPermissions))]
 namespace SPIXI.iOS.Classes
@@ -9,6 +10,14 @@ namespace SPIXI.iOS.Classes
     {
         public void requestAudioRecordingPermissions()
         {
+            AVAudioSession av_session = AVAudioSession.SharedInstance();
+            if (av_session != null)
+            {
+                av_session.RequestRecordPermission(delegate (bool granted)
+                {
+
+                });
+            }
         }
     }
 }
