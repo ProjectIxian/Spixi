d76e3ade3b4c6c364edbd1c6465235c3dd78c638
firestorm40 <cristian@firestorm.ro>
2021-10-21 20:42:49 +0200

    - added alternative authentication methods (biometric, pattern, text, etc) - added fallback authentication unlock based on wallet password - updated lock screen to include Change wallet option

--- SPIXI/SPIXI/Pages/Launch/LockPage.xaml.cs
+++ SPIXI/SPIXI/Pages/Launch/LockPage.xaml.cs
@@ -1,6 +1,12 @@
-﻿using SPIXI.Interfaces;
+﻿using IXICore;
+using Plugin.Fingerprint.Abstractions;
+using SPIXI.Interfaces;
+using SPIXI.Lang;
 using SPIXI.Meta;
 using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -8,19 +14,24 @@ using Xamarin.Forms.Xaml;
 namespace SPIXI
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class LockPage : SpixiContentPage
-	{
-		public LockPage ()
-		{
+    public partial class LockPage : SpixiContentPage
+    {
+        private CancellationTokenSource _cancel;
+        public LockPage()
+        {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
 
             loadPage(webView, "lock.html");
         }
 
-        private void onNavigated(object sender, WebNavigatedEventArgs e)
+        private async void onNavigated(object sender, WebNavigatedEventArgs e)
         {
+            if (Device.RuntimePlatform == Device.WPF)
+                return;
 
+            // Show biometric and alternative authentication methods
+            await AuthenticateAsync(SpixiLocalization._SL("global-lock-auth-text"));
         }
 
         private void onNavigating(object sender, WebNavigatingEventArgs e)
@@ -33,10 +44,17 @@ namespace SPIXI
             }
             else if (current_url.Contains("ixian:unlock:"))
             {
+                // Retrieve the password and unlock
                 string[] split = current_url.Split(new string[] { "ixian:unlock:" }, StringSplitOptions.None);
                 string pass = split[1];
-                if(pass != null)
+                if (pass != null)
                     doUnlock(pass);
+            }
+            else if (current_url.Equals("ixian:change", StringComparison.Ordinal))
+            {
+                // Show the launch screen
+                Navigation.PushAsync(new SPIXI.LaunchPage(), Config.defaultXamarinAnimations);
+                Navigation.RemovePage(this);
             }
             else
             {
@@ -50,6 +68,20 @@ namespace SPIXI
 
         private void doUnlock(string pass)
         {
+            string target_filepath = Path.Combine(Config.spixiUserFolder, Config.walletFile);
+            WalletStorage ws = new WalletStorage(target_filepath);
+            if (!ws.verifyWallet(target_filepath, pass))
+            {
+                displaySpixiAlert(SpixiLocalization._SL("intro-restore-file-invalidpassword-title"), SpixiLocalization._SL("intro-restore-file-invalidpassword-text"), SpixiLocalization._SL("global-dialog-ok"));
+            }
+            else
+            {
+                performUnlock();
+            }
+        }
+
+        private void performUnlock()
+        {
             Navigation.PushAsync(HomePage.Instance(true), Config.defaultXamarinAnimations);
             Navigation.RemovePage(this);
         }
@@ -58,5 +90,39 @@ namespace SPIXI
         {
             return true;
         }
+
+        private async Task AuthenticateAsync(string reason, string cancel = null, string fallback = null, string tooFast = null)
+        {
+            _cancel = new CancellationTokenSource();
+
+            var dialogConfig = new AuthenticationRequestConfiguration("SPIXI", reason)
+            {
+                CancelTitle = cancel,
+                FallbackTitle = fallback,
+                AllowAlternativeAuthentication = true,
+                ConfirmationRequired = true
+            };
+
+            dialogConfig.HelpTexts.MovedTooFast = tooFast;
+
+            var result = await Plugin.Fingerprint.CrossFingerprint.Current.AuthenticateAsync(dialogConfig, _cancel.Token);
+
+            await SetResultAsync(result);
+        }
+
+        private Task SetResultAsync(FingerprintAuthenticationResult result)
+        {
+            if (result.Authenticated)
+            {
+                performUnlock();
+            }
+            else
+            {
+                _ = displaySpixiAlert(SpixiLocalization._SL("global-lock-invalidpassword-title"), SpixiLocalization._SL("global-lock-invalidpassword-text"), "Cancel");
+            }
+
+            return Task.CompletedTask;
+        }
+
     }
 }
