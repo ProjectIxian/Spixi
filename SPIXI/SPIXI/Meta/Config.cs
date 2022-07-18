6c761aa4ce00a2120a35eae82105304cefd614a5
MarkoAT <no-reply@ixian.io>
2022-07-18 03:24:59 +0200

    spixi-0.6.11

--- SPIXI/SPIXI/Meta/Config.cs
+++ SPIXI/SPIXI/Meta/Config.cs
@@ -28,7 +28,7 @@ namespace SPIXI.Meta
         public static readonly int packetDataSize = 102400; // 100 Kb per packet for file transfers
         public static readonly long packetRequestTimeout = 60; // Time in seconds to re-request packets
 
-        public static readonly string version = "spixi-0.6.10"; // Spixi version
+        public static readonly string version = "spixi-0.6.11"; // Spixi version
 
         public static readonly string checkVersionUrl = "https://www.ixian.io/spixi-update.txt";
         public static readonly int checkVersionSeconds = 1 * 60 * 60; // 1 hour
