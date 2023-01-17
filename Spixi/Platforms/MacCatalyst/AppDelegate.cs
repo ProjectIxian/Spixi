b502c3a2aefc6d3822af5cb1e7b266ed641b9137
firestorm40 <cristian@firestorm.ro>
2023-01-17 20:55:26 +0200

    - added macOS support - updated project settings - fixed platform compile errors

--- Spixi/Platforms/MacCatalyst/AppDelegate.cs
+++ Spixi/Platforms/MacCatalyst/AppDelegate.cs
@@ -1,4 +1,8 @@
 ﻿using Foundation;
+using IXICore.Meta;
+using SPIXI.Lang;
+using SPIXI.Meta;
+using UIKit;
 
 namespace Spixi;
 
@@ -6,4 +10,76 @@ namespace Spixi;
 public class AppDelegate : MauiUIApplicationDelegate
 {
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+    public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+    {
+        UIApplication.SharedApplication.SetMinimumBackgroundFetchInterval(UIApplication.BackgroundFetchIntervalMinimum);
+
+        prepareStorage();
+
+        SpixiLocalization.addCustomString("Platform", "Xamarin-Mac");
+
+        return base.FinishedLaunching(app, options);
+    }
+
+    private void prepareStorage()
+    {
+        string source_html = Path.Combine(NSBundle.MainBundle.BundlePath, "Contents/Resources/html");
+        string dest_html = Path.Combine(Config.spixiUserFolder, "html");
+
+        if (!Directory.Exists(dest_html))
+        {
+            Directory.CreateDirectory(dest_html);
+        }
+
+        prepareSymbolicLinks(new DirectoryInfo(source_html), new DirectoryInfo(dest_html));
+    }
+
+    // Cleans up and links contents of the source directory to target directory.
+    private static void prepareSymbolicLinks(DirectoryInfo source, DirectoryInfo target)
+    {
+        var fm = new NSFileManager();
+        fm.ChangeCurrentDirectory(target.FullName);
+
+        NSError ns_error = new NSError();
+
+        foreach (DirectoryInfo dir in source.GetDirectories())
+        {
+            var tmp_path = Path.Combine(target.FullName, dir.Name);
+            if (Directory.Exists(tmp_path))
+            {
+                Directory.Delete(tmp_path, true);
+            }
+            if (File.Exists(tmp_path))
+            {
+                File.Delete(tmp_path);
+            }
+            fm.CreateSymbolicLink(dir.Name, dir.FullName, out ns_error);
+        }
+
+        foreach (FileInfo file in source.GetFiles())
+        {
+            var tmp_path = Path.Combine(target.FullName, file.Name);
+            if (Directory.Exists(tmp_path))
+            {
+                Directory.Delete(tmp_path, true);
+            }
+            if (File.Exists(tmp_path))
+            {
+                File.Delete(tmp_path);
+            }
+            fm.CreateSymbolicLink(file.Name, file.FullName, out ns_error);
+        }
+    }
+
+    public override void WillTerminate(UIApplication uiApplication)
+    {
+        IxianHandler.shutdown();
+        while (IxianHandler.status != NodeStatus.stopped)
+        {
+            Thread.Sleep(10);
+        }
+        base.WillTerminate(uiApplication);
+    }
+
 }
