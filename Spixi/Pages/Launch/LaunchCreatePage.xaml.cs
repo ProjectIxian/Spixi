40a27243f3d6e712020730d9108dbbae472c9cb4
firestorm40 <cristian@firestorm.ro>
2023-07-25 21:19:43 +0300

    - added image picker and image resize code for Windows platform

--- Spixi/Pages/Launch/LaunchCreatePage.xaml.cs
+++ Spixi/Pages/Launch/LaunchCreatePage.xaml.cs
@@ -53,9 +53,7 @@ namespace SPIXI
             }
             else if (current_url.Equals("ixian:avatar", StringComparison.Ordinal))
             {
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                onChangeAvatarAsync(sender, e);
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                _ = onChangeAvatarAsync(sender, e);
             }
             else
             {
--- Spixi/Platforms/Windows/SFilePicker.cs
+++ Spixi/Platforms/Windows/SFilePicker.cs
@@ -1,12 +1,8 @@
-﻿using IXICore.Meta;
+﻿using Microsoft.UI.Xaml.Media.Imaging;
 using SPIXI.Interfaces;
-using System;
-using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Spixi
 {
@@ -14,11 +10,20 @@ namespace Spixi
     {
         static TaskCompletionSource<SpixiImageData> taskCompletionSource;
 
-        public static Task<SpixiImageData> PickImageAsync()
+        public static async Task<SpixiImageData> PickImageAsync()
         {
+            var options = new PickOptions
+            {
+                FileTypes = FilePickerFileType.Images,
+            };
+            FileResult fileData = await FilePicker.PickAsync(options);
+            if (fileData == null)
+                return null; // User canceled file picking
+
+            SpixiImageData spixi_img_data = new() { name = Path.GetFileName(fileData.FullPath), path = fileData.FullPath, stream = await fileData.OpenReadAsync() };
+
             // Return Task object
-            taskCompletionSource = new TaskCompletionSource<SpixiImageData>();
-            return taskCompletionSource.Task;
+            return spixi_img_data;
         }
 
         public static async Task<SpixiImageData> PickFileAsync()
@@ -27,19 +32,54 @@ namespace Spixi
             if (fileData == null)
                 return null; // User canceled file picking
 
-            SpixiImageData spixi_img_data = new SpixiImageData() { name = Path.GetFileName(fileData.FullPath), path = fileData.FullPath, stream = await fileData.OpenReadAsync() };
+            SpixiImageData spixi_img_data = new() { name = Path.GetFileName(fileData.FullPath), path = fileData.FullPath, stream = await fileData.OpenReadAsync() };
 
             // Return Task object
             return spixi_img_data;
         }
 
-        
-
-        public static byte[] ResizeImage(byte[] image_data, int new_width, int new_height, int quality)
+        public static byte[] ResizeImage(byte[] imageData, int newWidth, int newHeight, long quality)
         {
-                return null;
+            using var originalImage = new Bitmap(new MemoryStream(imageData));
+
+            int originalWidth = originalImage.Width;
+            int originalHeight = originalImage.Height;
+
+            float widthRatio = (float)newWidth / originalWidth;
+            float heightRatio = (float)newHeight / originalHeight;
+
+            float ratio = Math.Max(widthRatio, heightRatio);
+
+            int resizedPreCropWidth = (int)Math.Round(originalWidth * ratio);
+            int resizedPreCropHeight = (int)Math.Round(originalHeight * ratio);
+
+            // Full area to crop on resized image
+            int resizedCropX = resizedPreCropWidth - newWidth;
+            int resizedCropY = resizedPreCropHeight - newHeight;
+
+            int croppedWidth = (int)((resizedPreCropWidth - resizedCropX) / ratio);
+            int croppedHeight = (int)((resizedPreCropHeight - resizedCropY) / ratio);
+
+            // Half of area to crop on original image
+            int cropX = (int)(resizedCropX / ratio / 2);
+            int cropY = (int)(resizedCropY / ratio / 2);
+
+            // Crop and resize
+            var resizedImage = new Bitmap(newWidth, newHeight);
+            using var graphics = Graphics.FromImage(resizedImage);
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.DrawImage(originalImage, new Rectangle(0, 0, newWidth, newHeight), new Rectangle(cropX, cropY, croppedWidth, croppedHeight), GraphicsUnit.Pixel);
+
+            // Convert to JPEG
+            var encoder = ImageCodecInfo.GetImageDecoders().First(codec => codec.FormatID == System.Drawing.Imaging.ImageFormat.Jpeg.Guid);
+            var encoderParameters = new EncoderParameters(1);
+            encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, quality);
+
+            using var outputStream = new MemoryStream();
+            resizedImage.Save(outputStream, encoder, encoderParameters);
+            return outputStream.ToArray();
         }
 
-      
+
     }
 }
