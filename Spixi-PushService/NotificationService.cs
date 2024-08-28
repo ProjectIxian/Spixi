c7b164ae9746198376e8f08adfe263c8d785e8d0
firestorm40 <cristian@firestorm.ro>
2024-08-28 14:01:49 +0300

    - added iOS Push Service project - updated OneSignal depedency to 5.2.1

---
+++ Spixi-PushService/NotificationService.cs
@@ -0,0 +1,41 @@
+﻿using Foundation;
+using OneSignalSDK.DotNet;
+using OneSignalSDK.DotNet.iOS;
+using System;
+using UIKit;
+using UserNotifications;
+
+namespace OneSignalNotificationServiceExtension
+{
+    [Register("NotificationService")]
+    public class NotificationService : UNNotificationServiceExtension
+    {
+        Action<UNNotificationContent> ContentHandler { get; set; }
+        UNMutableNotificationContent BestAttemptContent { get; set; }
+        UNNotificationRequest ReceivedRequest { get; set; }
+
+        protected NotificationService(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void DidReceiveNotificationRequest(UNNotificationRequest request, Action<UNNotificationContent> contentHandler)
+        {
+            ReceivedRequest = request;
+            ContentHandler = contentHandler;
+            BestAttemptContent = (UNMutableNotificationContent)request.Content.MutableCopy();
+
+            NotificationServiceExtension.DidReceiveNotificationExtensionRequest(request, BestAttemptContent, contentHandler);
+        }
+
+        public override void TimeWillExpire()
+        {
+            // Called just before the extension will be terminated by the system.
+            // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+
+            NotificationServiceExtension.ServiceExtensionTimeWillExpireRequest(ReceivedRequest, BestAttemptContent);
+
+            ContentHandler(BestAttemptContent);
+        }
+    }
+}
